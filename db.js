a42104863c9a67358c6b369f9de99b660937692c
AbscodInformatics <shivanshupathak@Shivanshus-MacBook-Air.local>
2023-11-23 16:49:55 +0530

    ths

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/my_database_new', { useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost/my_database_new', { useNewUrlParser: true });
 
+
+mongoose.connect('mongodb+srv://Shivanshu-pathak-effectual:sonxy9-pipviP-mecxir@cluster0.sdiwr.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true })
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
